a7abf878ba6d8ebe6904bc82007383a69932de52
Guilhermex3me <guilhermex3me@gmail.com>
2016-03-16 17:38:40 -0300

    Testes categoria

--- src/br/com/teste/yoshisisland/padaria/TesteCategorias.java
+++ src/br/com/teste/yoshisisland/padaria/TesteCategorias.java
@@ -6,46 +6,37 @@ import org.junit.Test;
 import br.com.yoshimachine.padaria.dao.CategoriaDao;
 import br.com.yoshimachine.padaria.model.Categoria;
 
-
-
 public class TesteCategorias {
 	@Test
 	public void testeInserirCategoria() {
-		
-		
-		
+
 		CategoriaDao dao = new CategoriaDao();
 		Categoria categoria = new Categoria();
 
 		categoria.setId_categoria(1);
 		categoria.setNome_categoria("doces");
-		categoria.setDescricao("é doce porque tem açucar demais");
-		
+		categoria.setDescricao("A doce porque tem aÃ§ucar demais");
 
-		int qtdInicio = dao.getListar().size();  
-		dao.getSalvar(categoria); 
-		int qtdFim = dao.getListar().size(); 
+		int qtdInicio = dao.getListar().size();
+		dao.getSalvar(categoria);
+		int qtdFim = dao.getListar().size();
 
 		Assert.assertEquals(qtdInicio + 1, qtdFim);
 	}
-	
+
 	@Test
-	public void testeRemoverCategoria(){
-		
-	CategoriaDao dao = new CategoriaDao();
-	Categoria categoria = new Categoria();
-	
-	categoria.setId_categoria(1);
-	int qtdInicio = dao.getListar().size();
-	dao.getSalvar(categoria);
-	int qtdFim = dao.getListar().size();
-	dao.close();
-	Assert.assertEquals(qtdInicio - 1, qtdFim);
-	
+	public void testeRemoverCategoria() {
+
+		CategoriaDao dao = new CategoriaDao();
+		Categoria categoria = new Categoria();
+
+		categoria.setId_categoria(1);
+		int qtdInicio = dao.getListar().size();
+		dao.getRemover(categoria.getId_categoria());
+		int qtdFim = dao.getListar().size();
+
+		Assert.assertEquals(qtdInicio - 1, qtdFim);
+
 	}
-	
 
-	
 }
-
-
